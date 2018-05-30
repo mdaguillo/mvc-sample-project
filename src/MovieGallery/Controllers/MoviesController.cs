3fd7ad7a9b7eb47e9eaaadf7638ec70b01201051
Mike D'Aguillo <mdaguillo@gmail.com>
2018-05-30 13:44:51 -0400

    Updated Movie Detail page to be strongly typed

--- src/MovieGallery/Controllers/MoviesController.cs
+++ src/MovieGallery/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,10 +11,18 @@ namespace MovieGallery.Controllers
 	{
 		public ActionResult Detail()
 		{
-			ViewBag.MovieTitle = "The Dark Knight";
-			ViewBag.ReleaseYear = 2008;
+			Movie movie = new Movie() {
+				Title = "The Dark Knight",
+				ReleaseDate = new DateTime(2008, 7, 18),
+				DescriptionHtml = "With the help of allies Lt. Jim Gordon (Gary Oldman) and DA Harvey Dent (Aaron Eckhart), Batman (Christian Bale) has been able to keep a tight lid on crime in Gotham City. But when a vile young criminal calling himself the Joker (Heath Ledger) suddenly throws the town into chaos, the caped Crusader begins to tread a fine line between heroism and vigilantism.",
+				Actors = new Actor[] { 
+					new Actor() { Name = "Christian Bale", Role ="Batman/Bruce Wayne" },
+					new Actor() { Name = "Heath Ledger", Role ="The Joker" },
+					new Actor() { Name = "Gary Oldman", Role ="Commissioner Gordon" }
+				}
+			};
 
-			return View();
+			return View(movie);
 		}
 	}
 }
--- src/MovieGallery/Models/Movie.cs
+++ src/MovieGallery/Models/Movie.cs
@@ -13,5 +13,14 @@ namespace MovieGallery.Models
 		public string DescriptionHtml { get; set; }
 		public Actor[] Actors { get; set; }
 		public bool Favorite { get; set; }
+
+		// title-year.jpg
+		public string CoverImageFileName 
+		{
+			get 
+			{
+				return Title.ToLower() + "-" + ReleaseDate.Year + ".jpg";
+			}
+		}
 	}
 }
