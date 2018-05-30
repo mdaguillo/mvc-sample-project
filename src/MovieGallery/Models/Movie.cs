e4c833a529e738e78f24912454979455da764d38
Mike D'Aguillo <mdaguillo@gmail.com>
2018-05-30 13:25:26 -0400

    Added Movie and Actor models

---
+++ src/MovieGallery/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieGallery.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public string Title { get; set; }
+		public string DescriptionHtml { get; set; }
+		public Actor[] Actors { get; set; }
+		public bool Favorite { get; set; }
+	}
+}
